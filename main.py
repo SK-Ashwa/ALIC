ca6a4b3aca3f850d3a52de9b9ef4daa79d89b209
SK-Ashwa <sohankumarsk.ashwa@gmail.com>
2024-04-14 20:22:45 +0530

    noninitial commit

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
-# Read the HTML file
-with open('D:\VS Code - 2\\ALIC\index.html', 'r') as file:
-    html_content = file.read()
+def run_html_file(html_file_path):
+    with open(html_file_path, "r") as f:
+        html_content = f.read()
+    st.components.v1.html(html_content, height=1000, scrolling=True)
 
-# Render the HTML
-components.html(html_content, height=600)
+def main():
+    st.title("Display HTML File in Streamlit")
+
+    html_file_path = "D:\VS Code - 2\ALIC\index.html"  # Update this with the path to your HTML file
+
+    run_html_file(html_file_path)
+
+if __name__ == "__main__":
+    main()
+
