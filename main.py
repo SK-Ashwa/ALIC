feb4767a277a193c7b86197862746d1b1b89f608
SK-Ashwa <sohankumarsk.ashwa@gmail.com>
2024-04-15 14:11:34 +0530

    noninitial commit

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import streamlit as st
-import streamlit.components.v1 as st
 
-def run_html_file(html_file_path):
-    with open(html_file_path, "r") as f:
-        html_content = f.read()
-    st.html(html_content, height=1000, scrolling=True)
+# Read the content of the index.html file
+with open("D:\\VS Code - 2\\ALIC\\index.html", "r") as file:
+    html_content = file.read()
 
-def main():
-    #st.write("Display HTML File in Streamlit")
-
-    html_file_path = "D:\VS Code - 2\ALIC\index.html"  # Update this with the path to your HTML file
-
-    run_html_file(html_file_path)
-
-if __name__ == "__main__":
-    main()
-
+# Display the content using components.iframe
+st.components.v1.html(html_content, height=600)
