6a9670d27f5ddceef4789e5c1cdc40160b3de22f
SK-Ashwa <166999666+SK-Ashwa@users.noreply.github.com>
2024-04-15 22:19:35 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Read the content of the index.html file
-with open("D:\\VS Code - 2\\ALIC\\index.html", "r") as file:
-    html_content = file.read()
-
-# Display the content using components.iframe
-st.components.v1.html(html_content, height=600)
